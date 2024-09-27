aa5959f94a803ff3e7fc877a7eaabad7270fc818
mvlassis <manos.vlassis@canonical.com>
2024-09-27 12:25:06 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
+append_alias() {
+	ALIAS_NAME=$1
+	ALIAS_COMMAND=$2
+	if ! grep -Fxq "$ALIAS_COMMAND" ~/.bashrc; then
+		echo "$ALIAS_COMMAND" >> ~/.bashrc
+		echo "Alias '$ALIAS_NAME' added to .bashrc. Please restart your terminal or run 'source ~/.bashrc' to apply the changes."
+	else
+		echo "Alias '$ALIAS_NAME' already exists in .bashrc."
+	fi
+}
+
+
 TARGET_DIR="~/shift-pro-coding/lessons"
+SHIFT_ALIAS="alias 'shift-cd'='cd $TARGET_DIR'"
 
-ALIAS_COMMAND="alias 'shift-cd'='cd $TARGET_DIR'"
+append_alias "shift-cd" SHIFT_ALIAS
 
-if ! grep -Fxq "$ALIAS_COMMAND" ~/.bashrc; then
-    echo "$ALIAS_COMMAND" >> ~/.bashrc
-    echo "Alias 'shift-cd' added to .bashrc. Please restart your terminal or run 'source ~/.bashrc' to apply the changes."
-else
-    echo "Alias 'shift-cd' already exists in .bashrc."
-fi
+append_alias "python" "alias python='python3"
